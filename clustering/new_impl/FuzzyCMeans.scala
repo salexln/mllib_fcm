bd39b28ec45ecc8776c10abd132d25390f11c83b
salexln <salexln@gmail.com>
2015-11-28 19:51:26 +0200

    adding new_impl - compiling

---
+++ clustering/new_impl/FuzzyCMeans.scala
@@ -0,0 +1,167 @@
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.mllib.clustering
+
+import org.apache.spark.Logging
+import org.apache.spark.util.random.XORShiftRandom
+
+// import org.apache.spark.mllib.clustering.KMeansModel
+import org.apache.spark.mllib.linalg.{Vectors, Vector}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
+import scala.collection.mutable.ArrayBuffer
+
+
+class FuzzyCKMeans private ( private var clustersNum: Int,
+                             private var maxIterations: Int,
+                             private var runs: Int,
+                             // private var initializationMode: String,
+                             // private var initializationSteps: Int,
+                             private var epsilon: Double,
+                             private var fuzzynessCoefficient: Double )
+// private var seed: Long)
+  extends Serializable with Logging {
+
+  // def this() = this(2, 20, 1, KMeans.K_MEANS_PARALLEL, 5, 1e-4, Utils.random.nextLong())
+  def this() = this(2, 20, 1, 1e-4, 2)
+
+  def getClustersNum: Int = this.clustersNum
+
+  def setClustersNum(clustersNum: Int): this.type = {
+    if (clustersNum <= 0) {
+      throw new IllegalArgumentException("Number of clusters must be positive")
+    }
+    this.clustersNum = clustersNum
+    this
+  }
+
+  def getMaxIterations: Int = this.maxIterations
+
+  def setMaxIterations(maxIter : Int): this.type = {
+    if (maxIter <= 0) {
+      throw new IllegalArgumentException("Number of max iterations must be positive")
+    }
+    this.maxIterations = maxIter
+    this
+  }
+
+  def getEpsilon: Double = this.epsilon
+
+  def setEplison(epsilon: Double): this.type = {
+    if (epsilon < 0 || epsilon > 1) {
+      throw new IllegalArgumentException("Epsilon value must be between 0 and 1")
+    }
+    this.epsilon = epsilon
+    this
+  }
+
+  def getRuns: Double = this.runs
+
+  def setRuns(runs: Int): this.type = {
+    this.runs = runs
+    this
+  }
+
+  def getFuzzynessCoefficient: Double = this.fuzzynessCoefficient
+
+  def setFuzzynessCoefficient(coeficient: Double): this.type = {
+    if (coeficient < 0) {
+      throw new IllegalArgumentException("Fuzzyness coefficient must be bigger than 1")
+    }
+    this.fuzzynessCoefficient = coeficient
+    this
+  }
+
+  def run(data: RDD[Vector]): FuzzyCMeansModel = {
+
+    if (data.getStorageLevel == StorageLevel.NONE) {
+      logWarning("The input data is not directly cached, which may hurt performance if its"
+        + " parent RDDs are also uncached.")
+    }
+
+    // Compute squared norms and cache them.
+    val norms = data.map(Vectors.norm(_, 2.0))
+    norms.persist()
+    val zippedData = data.zip(norms).map { case (v, norm) =>
+      new VectorWithNorm(v, norm)
+    }
+    val model = runAlgorithm(zippedData)
+    norms.unpersist()
+
+    // Warn at the end of the run as well, for increased visibility.
+    if (data.getStorageLevel == StorageLevel.NONE) {
+      logWarning("The input data was not directly cached, which may hurt performance if its"
+        + " parent RDDs are also uncached.")
+    }
+    model
+  }
+
+  /**
+    * Implementation of the Fuzzy C-Means algorithm
+    */
+  private def runAlgorithm(data: RDD[VectorWithNorm]): FuzzyCMeansModel = {
+
+    val sc = data.sparkContext
+    val initStartTime = System.nanoTime()
+
+    val numRuns = runs
+    val centers = initRandom(data)
+
+    // TODO - alex: this all should be in the KMeans base class
+//    val centers = initialModel match {
+//      case Some(fcmCenters) => {
+//        Array(fcmCenters.clusterCenters.map(s => new VectorWithNorm(s)))
+//      }
+//      case NONE => {
+//        if (initializationMode == KMeans.RANDOM) {
+//          initRandom(data)
+//        } else {
+//          initKMeansParaller(data);
+//        }
+//      }
+//    }
+
+    val initTimeInSeconds = (System.nanoTime() - initStartTime) / 1e9
+    logInfo("blablabla")
+
+
+    var activeRuns = new ArrayBuffer[Int] ++ (0 until numRuns)
+    var iteration = 0
+    val iterationStartTime = System.nanoTime()
+
+    while(iteration < maxIterations && !activeRuns.isEmpty) {
+      // ... the whole algorithm
+    }
+
+    val bestRun = 1;
+    new FuzzyCMeansModel(centers(bestRun).map(_.vector))
+  }
+
+  private def initRandom(data: RDD[VectorWithNorm])
+  : Array[Array[VectorWithNorm]] = {
+    // Sample all the cluster centers in one pass to avoid repeated scans
+
+    // create a random seed (maybe should be an input like in KMeans??
+    val sample = data.takeSample(true, runs * clustersNum, new XORShiftRandom().nextInt()).toSeq
+    Array.tabulate(runs)(r => sample.slice(r * clustersNum, (r + 1) * clustersNum).map { v =>
+      new VectorWithNorm(Vectors.dense(v.vector.toArray), v.norm)
+    }.toArray)
+  }
+
+}
---
+++ clustering/new_impl/FuzzyCMeansModel.scala
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.mllib.clustering
+
+import scala.collection.JavaConverters._
+
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
+
+import org.apache.spark.annotation.Since
+import org.apache.spark.api.java.JavaRDD
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.pmml.PMMLExportable
+import org.apache.spark.mllib.util.{Loader, Saveable}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.Row
+/**
+  * Created by development on 28/11/15.
+  */
+class FuzzyCMeansModel @Since("1.1.0") (@Since("1.0.0") val clusterCenters: Array[Vector])
+  extends Saveable with Serializable with PMMLExportable {
+
+
+  override protected def formatVersion: String = "1.0"
+
+  override def save(sc: SparkContext, path: String): Unit = {
+    // FuzzyCMeansModel.SaveLoadV1_0.save(sc, this, path)
+  }
+  /**
+    * A Java-friendly constructor that takes an Iterable of Vectors.
+    */
+  @Since("1.4.0")
+  def this(centers: java.lang.Iterable[Vector]) = this(centers.asScala.toArray)
+}
